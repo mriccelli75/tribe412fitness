5867fca0b762fb30a7e4e87242ce544c27e724a9
Mike Riccelli <mriccelli@ons.org>
2018-07-30 11:06:13 -0400

    more html img and css design work

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,3 @@ $(document).ready(function(){
     $('nav').toggleClass('active');
   });
 });
-$(document).ready(function(){
-var feed = new Instafeed ({
-  get: "tagged",
-  tagName: "tribe412fitness",
-  clientId: "bd560cf7f761454aa6d12dd5a8223386",
-  accessToken: "tribe412fitness/?426577602.1677ed0.d6e7d895c68e471e83093655204ef9ec"
-});
-feed.run();
-});
