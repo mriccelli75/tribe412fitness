6eaa513791ec503f665aa0158a347cc4dc481505
Mike Riccelli <mriccelli@ons.org>
2018-07-30 06:18:49 -0400

    more html css scaffolding

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,17 @@
 // custom.js
 
 //Navigation Menu Toggle
-
 $(document).ready(function(){
   $('.menu-toggle').click(function(){
     $('nav').toggleClass('active');
   });
+});
+$(document).ready(function(){
+var feed = new Instafeed ({
+  get: "tagged",
+  tagName: "tribe412fitness",
+  clientId: "bd560cf7f761454aa6d12dd5a8223386",
+  accessToken: "tribe412fitness/?426577602.1677ed0.d6e7d895c68e471e83093655204ef9ec"
+});
+feed.run();
 });
